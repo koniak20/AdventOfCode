1f3fccca75cbafa166bed7c022e12ff260d6f402
mwkowal <mwkowal@student.agh.edu.pl>
2021-12-10 07:21:04 +0100

    day10 cleaned up

--- Advent_of_code_2021/day10.py
+++ Advent_of_code_2021/day10.py
@@ -3,46 +3,28 @@ import os
 os.system("clear")
 
 if __name__ == '__main__':
-    with open("/home/koniak20/Downloads/input.txt") as fuck:
+    with open("./input_10/input_test.txt") as fuck:
         data = fuck.readlines()
-        # print(data)
-        stack = []
-        boolik = True
-        tab = ["[","<","{","("]
-        fir = {"[":"2","<":"4","{":"3","(":"1"}
+        fir = {"[":2,"<":4,"{":3,"(":1}
         sec = {"]":"[",">":"<","}":"{",")":"("}
+        points = {')': 3, ']': 57, '}': 1197, '>': 25137}
         result = 0
         scores = []
-        score = 0
         for line in data:
-            for char in line.replace("\n",""):
-                # print(stack)
-                if char in tab:
+            stack = []
+            for char in line.strip():
+                if char in sec.values():
                     stack.append(char)
-                else:
-                    if stack[-1] == sec[char]:
-                        stack.pop()
-                    else:
-                        if char == ")":
-                            result += 3
-                        elif char == "]":
-                            result += 57
-                        elif char == "}":
-                            result+= 1197
-                        elif char == ">":
-                            result+= 25137
-                        boolik = False
-                        break
-            if boolik:
+                elif not stack or stack.pop() != sec[char]:
+                    result += points[char]
+                    stack = []
+                    break
+            if stack:
+                score = 0
                 while len(stack) >0:
-                    score *= 5
-                    k = stack[-1]
-                    score += int(fir[k])
+                    score = 5*score + fir[stack[-1]]
                     stack.pop()
                 scores.append(score)
-                score = 0
-            boolik = True
-            stack.clear()
             # print(result)
         scores.sort()
         print(scores[int(len(scores)/2)])
